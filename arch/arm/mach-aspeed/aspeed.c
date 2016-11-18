7cb7afcae2e01eaf9853f02064de0e1cd367a978
doylehuang <doylehuang690712@gmail.com>
2016-11-18 15:37:54 +0800

    ARM: aspeed : sthelens: fix: Renable PWM1~6 multi-function
    The PWM function is disabled before, re-enable it.
    
    Merge conflict from foxconn-dev-4.4  , to be check
    
    Change-Id: I6446f1a8908273b24a97935ed31ed336020c9164
    Signed-off-by: doylehuang <doylehuang690712@gmail.com>
    Signed-off-by: SteveLinCH <dreamerch999@gmail.com>
    Signed-off-by: Steve Lin <steve.jc.lin@mail.foxconn.com>

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -105,7 +105,7 @@ static void __init do_sthelens_setup(void)
         writel(0x0370E677, AST_IO(AST_BASE_GPIO | 0x04));
 
         /* SCU setup */
-        writel(0x01C00000, AST_IO(AST_BASE_SCU | 0x88));//GPION1 - GPION6 should config to GPIO pin
+        writel(0x01C0007E, AST_IO(AST_BASE_SCU | 0x88));
         writel(0x003FA00C, AST_IO(AST_BASE_SCU | 0x90));//For MIDO2/MDC2
         writel(0x20000000, AST_IO(AST_BASE_SCU | 0x80)); //config GPIOF0, GPIOF1, GPIOF3, GPIOF7 to gpio pin
         /* To enable GPIOE0 pass through function debounce mode */
