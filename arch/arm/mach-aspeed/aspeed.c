3e36169390af9d11af4ea5d4d9730334b60b9601
Ken Lai <ken.sk.lai@foxconn.com>
2017-06-02 13:53:30 +0800

    Add support for Aspeed BMC SoCs
    Signed-off-by: Doyle Huang <doyle.sy.huang@mail.foxconn.com>
    
    Conflicts:
    	arch/arm/mach-aspeed/aspeed.c

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -147,7 +147,12 @@ static void __init do_zaius_setup(void)
 	reg = readl(AST_IO(AST_BASE_GPIO | 0x78));
 	board_rev = (reg >> 3) & 0x1F;
 	printk(KERN_INFO "Zaius platform board revision: 0x%02lx\n", board_rev);
+        /* Enable USB power*/
+        reg = readl(AST_IO(AST_BASE_GPIO | 0x7c));
+        writel(reg | BIT(14) , AST_IO(AST_BASE_GPIO | 0x7c));
 
+        reg = readl(AST_IO(AST_BASE_GPIO | 0x78));
+        writel(reg | BIT(14) , AST_IO(AST_BASE_GPIO | 0x78));
 	/* EVT boards have different FSI pin mappings */
 	if (board_rev < 0x08) {
 		static struct gpiod_lookup_table fsi_evt_gpio_lookup = {
@@ -223,6 +228,51 @@ static void __init do_zaius_setup(void)
 	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
 	/* Clear "Enable UART1 reset source from LPC" */
 	writel(reg & ~BIT(4), AST_IO(AST_BASE_LPC | 0x98));
+        /* USB HOST port 1 */
+        reg = readl(AST_IO(AST_BASE_SCU | 0x90));
+        writel(reg | BIT(29), AST_IO(AST_BASE_SCU | 0x90));
+
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg | BIT(14), AST_IO(AST_BASE_SCU | 0x04));
+
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x0C));
+        writel(reg | BIT(14), AST_IO(AST_BASE_SCU | 0x0C));
+	mdelay(10);
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg & ~BIT(14), AST_IO(AST_BASE_SCU | 0x04));
+
+        /* USB HOST port 1 */
+        reg = readl(AST_IO(AST_BASE_SCU | 0x94));
+        writel(reg & ~BIT(13), AST_IO(AST_BASE_SCU | 0x94));
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x94));
+        writel(reg | BIT(14), AST_IO(AST_BASE_SCU | 0x94));
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg | BIT(3), AST_IO(AST_BASE_SCU | 0x04));
+
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x0C));
+        writel(reg & ~BIT(7), AST_IO(AST_BASE_SCU | 0x0C));
+        mdelay(10);
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg & ~BIT(3), AST_IO(AST_BASE_SCU | 0x04));
+
+        /* USB HOST port 1 */
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg | BIT(15), AST_IO(AST_BASE_SCU | 0x04));
+
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x0C));
+        writel(reg & ~BIT(9), AST_IO(AST_BASE_SCU | 0x0C));
+        mdelay(10);
+
+        reg = readl(AST_IO(AST_BASE_SCU | 0x04));
+        writel(reg & ~BIT(15), AST_IO(AST_BASE_SCU | 0x04));
 }
 
 static void __init do_witherspoon_setup(void)
